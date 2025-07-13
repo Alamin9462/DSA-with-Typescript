97ac64387e6bcd196acec96729004445045148f8
Alamin9462 <alaminmd9462@gmail.com>
2025-07-13 20:56:05 +0600

    insertion Sorting

--- src/BubbleSort.ts
+++ src/BubbleSort.ts
@@ -14,35 +14,11 @@ const BubbleSort = (arrayWithBubbleSort:number[]): number[] => {
            swapped = true;
             }
         }
-        if(!swapped) break;
+        // if(!swapped) break;
     }
     return arrayWithBubbleSort;
        
 }
 
-console.log(BubbleSort([7, 44,10, 99, 22, 3, 9, 55, 12, 11]));
+console.log(BubbleSort([7, 44, 10, 99, 22, 3, 9, 55, 12, 11]));
 
-// const BubbleSort = (arrayWithBubbleSort: number[]): number[] => {
-//   const n = arrayWithBubbleSort.length;
-  
-//   for (let i = 0; i < n - 1; i++) {
-//     let swapped = false;
-
-//     for (let j = 0; j < n - i - 1; j++) {
-//       if (arrayWithBubbleSort[j] > arrayWithBubbleSort[j + 1]) {
-//         // ✅ Correct swap using j, not i
-//         const temp = arrayWithBubbleSort[j];
-//         arrayWithBubbleSort[j] = arrayWithBubbleSort[j + 1];
-//         arrayWithBubbleSort[j + 1] = temp;
-
-//         swapped = true;
-//       }
-//     }
-
-//     if (!swapped) break; // Optimization
-//   }
-
-//   return arrayWithBubbleSort;
-// };
-
-// console.log(BubbleSort([7, 3, 9, 12, 11]));
---
+++ src/insertionSort.ts
@@ -0,0 +1,18 @@
+
+const insertionSort = (arr: number[]) => {
+     const n = arr.length;
+     for(let i = 1; i < n; i++){
+       const curentValue = arr[i];
+        let j = i - 1;
+
+        while(j >= 0 && arr[j] > curentValue){
+          arr[j + 1] = arr[j];
+          j --;
+        }
+        arr[j + 1] = curentValue;
+     }
+     console.log(arr);
+}
+
+insertionSort([10, 5, 8, 9, 3, 11, 6, 8])
+
